0dbb49cb8e6b6ae452cae842c02ccaa24a7d0013
mkeller@arch <michaelkeller1997@gmail.com>
2025-10-17 13:34:40 -0400

    Updated to fix bug

--- addCommitMessageFormatter.sh
+++ addCommitMessageFormatter.sh
@@ -7,11 +7,12 @@ printf '#!/bin/sh\n'                                                    >> $file
 # get active branch name 
 printf 'BRANCH_NAME=$(git symbolic-ref --short HEAD)\n'                 >> $filePath
 # get Jira ticket number from branch 
-printf 'JIRA_TICKET=$(echo $BRANCH_NAME | grep -oE ' >> $filePath
-printf "'" >> $filePath
-printf '[A-Z]+-[0-9]+' >> $filePath
-printf "'" >> $filePath
-printf ')\n'  >> $filePath
+printf 'JIRA_TICKET=$(echo $BRANCH_NAME | grep -oE '                    >> $filePath
+printf "'"                                                              >> $filePath
+printf '[A-Z]+-[0-9]+'                                                  >> $filePath
+printf "'"                                                              >> $filePath
+printf ')\n'                                                            >> $filePath
+printf 'if [ ! -z "$JIRA_TICKET" ]; then'                               >> $filePath
 printf '\tCOMMIT_MSG=$(cat $1)\n'                                       >> $filePath
 printf '\techo "$JIRA_TICKET $COMMIT_MSG" > $1\n'                       >> $filePath
 printf 'fi\n'                                                           >> $filePath
