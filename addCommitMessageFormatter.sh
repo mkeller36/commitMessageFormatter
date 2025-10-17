b5baa46e462bfec8006e2d8784043f4ec40a7a0d
mkeller@arch <michaelkeller1997@gmail.com>
2025-10-17 13:30:51 -0400

    Initial commit

---
+++ addCommitMessageFormatter.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+filePath=".git/hooks/prepare-commit-msg"
+touch $filePath
+chmod +x $filePath
+printf '#!/bin/sh\n'                                                    >> $filePath
+# get active branch name 
+printf 'BRANCH_NAME=$(git symbolic-ref --short HEAD)\n'                 >> $filePath
+# get Jira ticket number from branch 
+printf 'JIRA_TICKET=$(echo $BRANCH_NAME | grep -oE ' >> $filePath
+printf "'" >> $filePath
+printf '[A-Z]+-[0-9]+' >> $filePath
+printf "'" >> $filePath
+printf ')\n'  >> $filePath
+printf '\tCOMMIT_MSG=$(cat $1)\n'                                       >> $filePath
+printf '\techo "$JIRA_TICKET $COMMIT_MSG" > $1\n'                       >> $filePath
+printf 'fi\n'                                                           >> $filePath
+
+rm ./addCommitMessageFormatter.sh
