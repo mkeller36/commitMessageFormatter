4d56cd3b864b8612c05825b5e00cfa953b75611d
mkeller@arch <michaelkeller1997@gmail.com>
2025-10-17 15:25:31 -0400

    more Formatting

--- addCommitMessageFormatter.sh
+++ addCommitMessageFormatter.sh
@@ -15,7 +15,7 @@ printf ')\n'                                                            >> $file
 # add jira ticket number to front of message 
 printf 'if [ ! -z "$JIRA_TICKET" ]; then\n'                             >> $filePath
 printf '\tCOMMIT_MSG=$(cat $1)\n'                                       >> $filePath
-printf '\techo "JIRA_CLOUD: $JIRA_TICKET $COMMIT_MSG" > $1\n'                       >> $filePath
+printf '\techo "JIRA_CLOUD: $JIRA_TICKET $COMMIT_MSG" > $1\n'           >> $filePath
 printf 'fi\n'                                                           >> $filePath
 
 # remove formatter program
